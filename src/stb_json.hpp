47007f384c2202efdae67119e49d8c23fdd5e5a5
Ricard Pillosu <d0n3val@gmail.com>
2019-07-23 10:36:55 +0200

    Added C++ wrapper class

---
+++ src/stb_json.hpp
@@ -0,0 +1,54 @@
+// Example of a wrapper C++ class for stb_json
+//
+// TODO:
+// Use of std::string instead of char buffers
+// Throw exceptions for error handling (?)
+//
+
+#ifndef STB_JSON_HPP
+#define STB_JSON_HPP
+
+#define STB_JSON_IMPLEMENTATION
+#include "stb_json.h"
+
+class stb_json
+{
+    public:
+        stb_json(const char* buffer, int buffer_length) { context = stbj_create_context(buffer, buffer_length); }
+        stb_json(const stbj_context& context) : context(context) {}
+
+        // Parsing methods ------------
+        void GetString(const char* field_name, char* dest_buffer, int dest_size, const char* default_value = "?") const
+        { stbj_read_string(&context, field_name, dest_buffer, dest_size, default_value); }
+
+        void GetString(int index, char* dest_buffer, int dest_size, const char* default_value = "?") const
+        { stbj_readp_string(&context, index, dest_buffer, dest_size, default_value); }
+
+        int GetInt(const char* field_name, int default_value = 0) const
+        { return stbj_read_int(&context, field_name, default_value); }
+
+        int GetInt(int index, int default_value = 0) const
+        { return stbj_readp_int(&context, index, default_value); }
+
+        double GetDouble(const char* field_name, double default_value = 0.0) const
+        { return stbj_read_double(&context, field_name, default_value); }
+
+        double GetDouble(int index, double default_value = 0.0) const
+        { return stbj_readp_double(&context, index, default_value); }
+
+        stb_json MoveCursor(const char* field_name) const
+        { return stbj_context(stbj_read_context(&context, field_name)); }
+
+        stb_json MoveCursor(int index) const
+        { return stbj_context(stbj_readp_context(&context, index)); }
+
+        // Helper methods ------------
+        int Count() const { return stbj_count_elements(&context); }
+        const char* GetError() const { return stbj_get_last_error(&context); }
+        bool HasError() const { return stbj_is_error(&context); }
+
+    private:
+        mutable stbj_context context;
+};
+
+#endif // STB_JSON_HPP
---
+++ test/cpp/test_class.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../src/stb_json.hpp"
+
+#define KB 1024
+#define MB 1048576
+
+int main()
+{
+
+    printf("Testing C++ wrapper to stb_json\n");
+
+    {
+        // Huge JSON test ---------------------
+        char buf[3*MB];
+        char str[100];
+
+        printf("READING CANADA.JSON ---------------------\n");
+        FILE* fp = fopen("canada.json", "rb");
+        if(fp)
+        {
+            int len = fread(buf, 1, 3*MB, fp);
+            printf("canada.json opened correctly, size %i\n", len);
+            fclose(fp);
+
+            stb_json json(buf, len);
+            int count = json.Count();
+            printf("num element from root: %i\n", count);
+
+            if(json.HasError()) 
+                printf("ERROR: %s\n", json.GetError());
+
+            json.GetString("type", str, 100);
+
+            printf("Type : %s\n", str);
+
+            json = json.MoveCursor("features");
+            json = json.MoveCursor(0);
+
+            if(json.HasError()) 
+                printf("ERROR: %s\n", json.GetError());
+
+            json.GetString("type", str, 100);
+            printf("-type : %s\n", str);
+
+            json = json.MoveCursor("geometry");
+            json = json.MoveCursor("coordinates");
+
+            if(json.HasError()) 
+                printf("ERROR: %s\n", json.GetError());
+
+            count = json.Count();
+
+            for(int a=0; a < count; ++a)
+            {
+                stb_json json2 = json.MoveCursor(a);
+                int count2 = json2.Count();
+
+                printf("Shape %i contains %i coords\n", a, count2);
+
+                for(int b=0; b < count2; ++b)
+                {
+                    stb_json json3 = json2.MoveCursor(b);
+                    printf(">>> Coord %i: %f %f\n", b, 
+                            json3.GetDouble(0), json3.GetDouble(1));
+                }
+            }
+        }
+        else
+            printf("Could not open canada.json\n");
+    }
+
+    {
+        // Huge JSON test 2 ---------------------
+        char buf[2*MB];
+        char str[100];
+
+        printf("READING CITM_CATALOG.JSON ---------------------\n");
+        FILE* fp = fopen("citm_catalog.json", "rb");
+        if(fp)
+        {
+            int len = fread(buf, 1, 2*MB, fp);
+            printf("citm_catalog.json opened correctly, size %i\n", len);
+            fclose(fp);
+
+            stb_json json(buf, len);
+            int count = json.Count();
+            printf("Num element from root: %i\n", count);
+
+            if(json.HasError()) 
+                printf("ERROR: %s\n", json.GetError());
+
+            json = json.MoveCursor("events");
+            count = json.Count();
+            printf("Num element on events: %i\n", count);
+
+            if(json.HasError()) 
+                printf("ERROR: %s\n", json.GetError());
+
+            for(int a=0; a < count; ++a)
+            {
+                stb_json json2 = json.MoveCursor(a);
+
+                json2.GetString("name", str, 100);
+                printf("Event %i -----------\nName: %s\n", a, str);
+
+                json2.GetString("description", str, 100);
+                printf("Description: %s\n", str);
+
+                json2.GetString("logo", str, 100);
+                printf("Logo: %s\n", str);
+
+                printf("Id: %i\n", json2.GetInt("id"));
+
+                if(json.HasError()) 
+                    printf("ERROR: %s\n", json.GetError());
+            }
+        }
+        else
+            printf("Could not open citm_catalog.json\n");
+    }
+}
+
